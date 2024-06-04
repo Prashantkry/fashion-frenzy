43e1576da41afb2514fa50e21c409ffcd8dc6d47
Prashant Kumar <71703153+Prashantkry@users.noreply.github.com>
2024-06-04 12:32:44 +0530

    navbar modified

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -62,7 +62,7 @@ export default function Navbar() {
               <li>
                 {isLoggedIn ? (
                   <button
-                    className="text-gray-800 hover:bg-gray-300 focus:ring-4 focus:ring-gray-400 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                    className="text-gray-800 hover:text-blue-600 focus:ring-4 focus:ring-gray-400 font-medium rounded-lg text-sm  border-0 mr-2 focus:outline-none"
                     onClick={handleSignOut}
                   >
                     Sign Out
